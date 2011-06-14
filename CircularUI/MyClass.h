1528284e738d0942f0a25dd1bace89c3a70df5c8
flapiello <flapiello@aol.com>
2011-06-14 22:41:30 +0200

    Test

---
+++ CircularUI/MyClass.h
@@ -0,0 +1,16 @@
+//
+//  MyClass.h
+//  CircularUI
+//
+//  Created by Fabrizio Lapiello on 14/06/11.
+//  Copyright 2011 Fabrizio Lapiello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyClass : TestObj {
+    
+}
+
+@end
---
+++ CircularUI/MyClass.m
@@ -0,0 +1,14 @@
+//
+//  MyClass.m
+//  CircularUI
+//
+//  Created by Fabrizio Lapiello on 14/06/11.
+//  Copyright 2011 Fabrizio Lapiello. All rights reserved.
+//
+
+#import "MyClass.h"
+
+
+@implementation MyClass
+
+@end
